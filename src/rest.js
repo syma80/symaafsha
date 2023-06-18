a5f8e3a97a0421831adc506e954825a796513ddd
HP <symaafsha80@gmail.com>
2023-06-18 14:18:07 -0700

    ----oooooouuu

--- src/rest.js
+++ src/rest.js
@@ -1,7 +1,7 @@
 import React from "react";
-import { Button, Col, Row} from "react-bootstrap";
+import { Button, Col, Container, Row} from "react-bootstrap";
 import { Link } from "react-router-dom";
-
+import './rest.css';
 
 export default function Rest(){
     return(
@@ -12,13 +12,16 @@ export default function Rest(){
 
 
                       <p style={{color: "white", fontFamily: "serif", fontSize:19}}>To follow my work at <a  style={{ color: "#cf2e2e"}} href="https://www.researchgate.net/profile/Syma-Afsha" target="_blank">ResearchGate</a>.</p>
-                  <Row>
-                  <Col xs={6} sm={6} lg={4} >
-            <Link style={{color: "white", textDecoration: "none"}} to ="/about-me"><Button variant="outline-primary" className="home-button">Read More</Button></Link></Col>
-            <Col xs={6} sm={6} lg={4} >
-            <a style={{color: "white", textDecoration: "none"}} href="https://drive.google.com/file/d/129mDal8P_cEGxH5xxcqBfxBRQvDptgJE/view?usp=sharing" target="_blank"> <Button variant="outline-primary" className="home-button">My Resume</Button></a></Col>
+                    
+                      <Row >
+                  <Col xs={6} sm={6} md={4} lg={4}>
+            <Link style={{color: "white", textDecoration: "none"}} to ="/about-me"><Button variant="outline-primary" className="home-button" >Read More</Button></Link></Col>
+            <Col  xs={6} sm={6} md={4} lg={4} >
+            <a style={{color: "white", textDecoration: "none"}} href="https://drive.google.com/file/d/129mDal8P_cEGxH5xxcqBfxBRQvDptgJE/view?usp=sharing" target="_blank"> <Button variant="outline-primary" className="home-button" >My Resume</Button></a></Col>
             
                   </Row>
+                     
+                 
                       
         </div>
         
