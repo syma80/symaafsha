d469877bac6ac9c3f599167c373ef5b1885528b5
Syma Afsha <65045231+syma80@users.noreply.github.com>
2023-06-18 11:53:11 -0700

    Add files via upload

--- src/rest.js
+++ src/rest.js
@@ -14,9 +14,9 @@ export default function Rest(){
                       <p style={{color: "white", fontFamily: "serif", fontSize:19}}>To follow my work at <a  style={{ color: "#cf2e2e"}} href="https://www.researchgate.net/profile/Syma-Afsha" target="_blank">ResearchGate</a>.</p>
                   <Row>
                   <Col xs={6} sm={6} lg={4} >
-            <Link style={{color: "white", textDecoration: "none"}} to ="/about"><Button variant="outline-primary" style={{color:"white", fontSize: 18, textAlign: "center", width: 130, height: 50, fontFamily: "serif"}}className="home-button">Read More</Button></Link></Col>
+            <Link style={{color: "white", textDecoration: "none"}} to ="/about-me"><Button variant="outline-primary" style={{color:"white", fontSize: 18, textAlign: "center", width: 130, height: 60, fontFamily: "serif"}}className="home-button">Read More</Button></Link></Col>
             <Col xs={6} sm={6} lg={4} >
-            <a style={{color: "white", textDecoration: "none"}} href="https://drive.google.com/file/d/129mDal8P_cEGxH5xxcqBfxBRQvDptgJE/view?usp=sharing" target="_blank"> <Button variant="outline-primary" style={{color:"white", fontSize: 18, textAlign: "center", width: 130, height: 50, fontFamily: "serif"}}className="home-button">My Resume</Button></a></Col>
+            <a style={{color: "white", textDecoration: "none"}} href="https://drive.google.com/file/d/129mDal8P_cEGxH5xxcqBfxBRQvDptgJE/view?usp=sharing" target="_blank"> <Button variant="outline-primary" style={{color:"white", fontSize: 18, textAlign: "center", width: 130, height: 60, fontFamily: "serif"}}className="home-button">My Resume</Button></a></Col>
             
                   </Row>
                       
